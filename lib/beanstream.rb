7d8647e38bcc8da2fbd087a61ab9da816cc8b830
jfparentcbx <38502346+jfparentcbx@users.noreply.github.com>
2018-04-18 13:39:06 -0400

    ACE-155 - Update bambora url (#1)

--- lib/beanstream.rb
+++ lib/beanstream.rb
@@ -6,38 +6,37 @@ require 'beanstream/util'
 require 'beanstream/exceptions'
 
 module Beanstream
-  
-  @url_prefix = "www"
-  @url_suffix = "api"
-  @url_base = "beanstream.com"
+
+  @url_prefix = "tls12-api"
+  @url_base = "na.bambora.com"
   @url_version = "v1"
   @ssl_ca_cert = File.dirname(__FILE__) + '/resources/cacert.pem'
   @timeout = 80
   @open_timeout = 40
-  
+
   class << self
     attr_accessor :merchant_id, :payments_api_key, :profiles_api_key, :reporting_api_key
-    attr_accessor :url_prefix, :url_base, :url_suffix, :url_version
+    attr_accessor :url_prefix, :url_base, :url_version
     attr_accessor :url_payments, :url_return, :url_void
     attr_accessor :ssl_ca_cert, :timeout, :open_timeout
   end
-  
+
   def self.api_host_url()
     "https://#{@url_prefix}.#{url_base}"
   end
-  
+
   def self.api_base_url()
-    "/#{url_suffix}/#{url_version}"
+    "/#{url_version}"
   end
-  
+
   def self.PaymentsAPI()
     Beanstream::PaymentsAPI.new()
   end
-  
+
   def self.ProfilesAPI()
     Beanstream::ProfilesAPI.new()
   end
-  
+
   def self.ReportingAPI()
     Beanstream::ReportingAPI.new()
   end
